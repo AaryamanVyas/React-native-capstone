b90b66e082edc9024439380d4d02806d575c48e2
aaryamantriescode <aaryamanvyas1@gmail.com>
2025-09-01 23:37:32 +0530

    .

--- capstone/App.js
+++ capstone/App.js
@@ -1,15 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
+import { AppRegistry } from 'react-native';
 import Onboarding from './screens/Onboarding';
-const Stack=createNativeStackNavigator();
-export default function App(){
-    return(
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name='Onboarding' component={Onboarding}/>
-            </Stack.Navigator>
-            <Onboarding/>
-        </NavigationContainer>
-    )
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Onboarding"
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen 
+          name="Onboarding" 
+          component={Onboarding}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+AppRegistry.registerComponent('main', () => App);
+
+export default App;
